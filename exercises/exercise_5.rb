3b02e5ae6bde11879b15ebcbcce78401ed7a5d22
DustinFader <dustinfader16@gmail.com>
2024-03-09 00:03:28 -0400

    finish exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,8 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts Store.sum(:annual_revenue)
+# average annual store amount
+puts Store.sum(:annual_revenue) / Store.count
+# amount of stores greater than 1m
+puts Store.where(["annual_revenue >= ?", 1000000]).size
